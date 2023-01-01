de64a157bf3750de4315b4c5568ff18f4e8942b0
Andrew Belko <spydnet@gmail.com>
2023-01-01 22:25:41 +0300

    gitignore best practice + local.tf for init-plan-apply-destroy

--- terraform-local-file/local.tf
+++ terraform-local-file/local.tf
@@ -1,4 +1,5 @@
 resource "local_file" "pet" {
-  filename = "/Users/andrew/pets.txt"
-  content = "We love pets!"  
+  filename = "pets.txt"
+  content = "We love pets!"
+  file_permission = "0700"
 }
