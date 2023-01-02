fafa0a03f7873b25f67e3755660053e477eaf268
Andrew Belko <spydnet@gmail.com>
2023-01-02 19:00:06 +0300

    dependencies

--- terraform-local-file/main.tf
+++ terraform-local-file/main.tf
@@ -2,6 +2,10 @@ resource "local_file" "pet" {
   filename = var.filename
   content = "We love ${random_pet.my-pet.id}! Time: ${time_static.time-stamp.id}"
   file_permission = "0700"
+  depends_on = [
+    time_static.time-stamp,
+    random_pet.my-pet
+  ]
 }
 
 resource "random_pet" "my-pet" {
