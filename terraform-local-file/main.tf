75a87be8d636f52965d4f16492663e9d9a54120f
Andrew Belko <spydnet@gmail.com>
2023-01-02 18:34:14 +0300

    lecture progress update

---
+++ terraform-local-file/main.tf
@@ -0,0 +1,11 @@
+resource "local_file" "pet" {
+  filename = var.filename
+  content = "We love ${random_pet.my-pet.id}!"
+  file_permission = "0700"
+}
+
+resource "random_pet" "my-pet" {
+  prefix = var.prefix
+  separator = var.separator
+  length = var.length
+}
