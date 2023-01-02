c84b3c79ca99a5a30420948a3b54630e1b4bce45
Andrew Belko <spydnet@gmail.com>
2023-01-02 18:55:52 +0300

    time_static resource

--- terraform-local-file/main.tf
+++ terraform-local-file/main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "pet" {
   filename = var.filename
-  content = "We love ${random_pet.my-pet.id}!"
+  content = "We love ${random_pet.my-pet.id}! Time: ${time_static.time-stamp.id}"
   file_permission = "0700"
 }
 
@@ -8,4 +8,7 @@ resource "random_pet" "my-pet" {
   prefix = var.prefix
   separator = var.separator
   length = var.length
+}
+
+resource "time_static" "time-stamp" {
 }
