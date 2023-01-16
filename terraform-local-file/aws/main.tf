c687482a7f4306e70cc0a8ca4249dc2ac10ea86d
Andrew Belko <spydnet@gmail.com>
2023-01-16 18:05:39 +0300

    policy attachments

--- terraform-local-file/aws/main.tf
+++ terraform-local-file/aws/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-west-2"
-  # access_key = "test"
-  # secret_key = "test"
+  # access_key = "test" in .aws/config
+  # secret_key = "test" export AWS_SECRET_ACCESS_KEY_ID=
 }
 
 resource "aws_iam_user" "admin-user" {
@@ -9,4 +9,14 @@ resource "aws_iam_user" "admin-user" {
   tags = {
     Description = "Technical Team leader"
   }
+}
+
+resource "aws_iam_policy" "adminUser" {
+  name   = "AdminUsers"
+  policy = file("admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "lucy-admin-access" {
+  user       = aws_iam_user.admin-user.name
+  policy_arn = aws_iam_policy.adminUser.arn
 }
